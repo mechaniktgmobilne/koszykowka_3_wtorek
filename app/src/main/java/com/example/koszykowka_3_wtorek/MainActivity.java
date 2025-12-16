242b026aac60c8c928e3644ccbd4906757ad09d0
mechaniktgmobilne <marzkup@gmail.com>
2025-12-16 08:17:20 +0100

    punkty bez ViewModel

--- app/src/main/java/com/example/koszykowka_3_wtorek/MainActivity.java
+++ app/src/main/java/com/example/koszykowka_3_wtorek/MainActivity.java
@@ -10,12 +10,41 @@ import com.example.koszykowka_3_wtorek.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;//klasa z bindowanymi widokami
     //dla pliku activity_main.xml ActivityMAinBinding
+    private int punkty = 0 ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-        binding.textView.setText(0);
+        binding.textView.setText("0");
+
+        binding.button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty++;
+                        binding.textView.setText(""+punkty);
+                    }
+                }
+        );
+        binding.button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty+=2;
+                        binding.textView.setText(""+punkty);
+                    }
+                }
+        );
+        binding.button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty+=3;
+                        binding.textView.setText(""+punkty);
+                    }
+                }
+        );
     }
 }
---
+++ app/src/main/java/com/example/koszykowka_3_wtorek/PunktyViewModel.java
@@ -0,0 +1,7 @@
+package com.example.koszykowka_3_wtorek;
+
+import androidx.lifecycle.ViewModel;
+
+public class PunktyViewModel extends ViewModel {
+    private int punkty;
+}
