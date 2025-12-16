23b1fa946142d65202f04bee74633ac34c7a3bcc
mechaniktgmobilne <marzkup@gmail.com>
2025-12-16 08:09:37 +0100

    wiazanie widokow

--- app/src/main/java/com/example/koszykowka_3_wtorek/MainActivity.java
+++ app/src/main/java/com/example/koszykowka_3_wtorek/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.koszykowka_3_wtorek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.koszykowka_3_wtorek.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding;//klasa z bindowanymi widokami
+    //dla pliku activity_main.xml ActivityMAinBinding
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.textView.setText(0);
     }
 }
